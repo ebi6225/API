550d509cfdfab778fad4e1373b4fc7e965ab9b50
Ebrahim <ebrahim.sedighi@consolsys.com>
2020-07-14 09:12:17 +0800

    First commit

---
+++ Controllers/CallAction.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [EnableCors("AllowOrigin")]
+    public class CallAction : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "API Is Running";
+        }
+
+        [HttpPost]
+        [Route("TSPRoute")]
+        public string PostTest()
+        {
+            return "D";
+        }
+
+        [HttpPost]
+        [Route("TSPRoute1")]
+        public string PostTest1()
+        {
+            return "D";
+        }
+    }
+}
