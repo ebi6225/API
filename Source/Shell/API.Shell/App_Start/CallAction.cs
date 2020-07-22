9214262b7842bd72f989bee8774aa8276f9c73f6
Ebrahim <ebrahim.sedighi@consolsys.com>
2020-07-22 13:52:37 +0800

    Prepare db connection

--- Source/Shell/API.Shell/App_Start/CallAction.cs
+++ Source/Shell/API.Shell/App_Start/CallAction.cs
@@ -1,14 +1,25 @@
 ﻿using API.Controller.Controllers;
+using Base.Web.API.Managers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace API.Shell.App_Start
 {
     public class CallAction : AbstractController
     {
+        public CallAction(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+
         [HttpGet]
         public string Get()
         {
-            return "API Is Running";
+            string connectionString = Configuration.GetConnectionString("Name");
+
+            return AppSettingManager.Instance.Get<string>("APIMessage");
         }
     }
 }
